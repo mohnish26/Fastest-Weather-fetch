65d8ced795c078efd36e7846ca166ab87a5e79ee
mohnish khanna <33591475+mohnish26@users.noreply.github.com>
2023-12-27 21:33:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [country, setCountry] = useState(null)
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Mumbai");
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=fe4feefa8543e06d4f3c66d92c61b69c&units=metric`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      // console.log(resJson);
+      setCity(resJson.main);
+      setCountry(resJson.sys)
+    };
+
+    fetchApi();
+  }, [search]);
+
+  return (
+    <><div className="serchdiv">
+      <input
+        placeholder="Fastest Serch"
+        type="search"
+        className="inputFeild"
+        onChange={(event) => { setSearch(event.target.value) }} />
+    </div>
+      <div className="maindiv">
+
+
+        {!city && !country ? (
+          <p>No Data Found</p>) : (
+          <div>
+            <div className="heading">
+              {search.toUpperCase()}
+            </div>
+            <h1 className="temp">{city.temp}</h1>
+            <h3 className="tempmin_max"> Min : {city.temp_min}° C |   Max: {city.temp_max}° C</h3>
+            <h3 className="tempmin_max">Country {country.country} </h3>
+          </div>
+
+        )}
+
+      </div>
+
+
+    </>
+  );
+}
+
+export default App;
